73de46c0de56e73a979e2eb5e6f27dbbe3c81905
OGecgo <oian72116@gmail.com>
2024-12-26 21:30:55 +0200

    leftPanel

--- ΑΑΥ_ΕΡΓΑΣΙΑ_3/tentpal/src/pages/Home.js
+++ ΑΑΥ_ΕΡΓΑΣΙΑ_3/tentpal/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // image
-import image from "../assets/logo.svg";
+import image from "../assets/bPanel.webp";
 
 // css
 import "../styles/Home.css";
@@ -10,13 +10,13 @@ import "../styles/Home.css";
 function Home({ navigate }) {
   return (
     <>
+      <img class = "leftPanel" src={image} alt="logo" />
       <div class = "center">
         <h1>Home Page</h1><br/>
         <p>Welcome to my website</p><br/>
         <button onClick={() => navigate("Html")}>Go to About</button>
-        
       </div>
-      <img class = "leftPanel" src={image} alt="logo" />
+      
     </>
 
     
