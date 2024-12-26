3643905ec7307129703d0b03f8f30205686eb9c7
OGecgo <oian72116@gmail.com>
2024-12-26 20:52:03 +0200

    testImages

--- ΑΑΥ_ΕΡΓΑΣΙΑ_3/tentpal/src/pages/Home.js
+++ ΑΑΥ_ΕΡΓΑΣΙΑ_3/tentpal/src/pages/Home.js
@@ -1,11 +1,25 @@
 import React from "react";
 
+// image
+import image from "../assets/logo.svg";
+
+// css
+import "../styles/Home.css";
+
+
 function Home({ navigate }) {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <button onClick={() => navigate("Html")}>Go to About</button>
-    </div>
+    <>
+      <div class = "center">
+        <h1>Home Page</h1><br/>
+        <p>Welcome to my website</p><br/>
+        <button onClick={() => navigate("Html")}>Go to About</button>
+        
+      </div>
+      <img class = "leftPanel" src={image} alt="logo" />
+    </>
+
+    
   );
 }
 
