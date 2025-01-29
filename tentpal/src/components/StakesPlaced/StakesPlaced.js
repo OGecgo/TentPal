1a58c57fcd23fabd70fbe5c3b7e656d6ebfc17f0
OGecgo <oian72116@gmail.com>
2025-01-30 01:02:41 +0200

    memmory for every stake

--- tentpal/src/components/StakesPlaced/StakesPlaced.js
+++ tentpal/src/components/StakesPlaced/StakesPlaced.js
@@ -7,29 +7,43 @@ import Environmental from "./Environmental/Environmental";
 import Panel from "./Panel/Panel";
 import Tent from "./Tent/Tent";
 
+let stakeVar = [50, 10, 20, 90];
+let stakeUse = [true, false, false, false];
+
 function StakesPlaced() {
 
     const [deepPres, setDeepPres] = useState(30);
-    const [Steke, SetSteke] = useState(1);
+    const [stake, SetStake] = useState(1);
     const deepRealPres = 30 + deepPres / 7;
 
+    // remeber the stakes
+    for (let i = 0; i < 4; i++) {
+        if (stakeUse[i]) {
+            stakeVar[i] = deepPres;
+        }
+    }
+
     useEffect(() => {
-        playStake(Steke);
-    }, [Steke]);
+        playStake(stake);
+    }, [stake]);
 
     const playStake = (num) => {
         switch(num){
             case 1:
-                setDeepPres(30);
+                setDeepPres(stakeVar[0]);
+                stakeUse = [true, false, false, false];
                 break;
             case 2:
-                setDeepPres(40);
+                setDeepPres(stakeVar[1]);
+                stakeUse = [false, true, false, false];
                 break;    
             case 3:
-                setDeepPres(50);
+                setDeepPres(stakeVar[2]);
+                stakeUse = [false, false, true, false];
                 break;
             case 4:
-                setDeepPres(60);
+                setDeepPres(stakeVar[3]);
+                stakeUse = [false, false, false, true];
                 break;
             default:
                 break;
@@ -40,7 +54,7 @@ function StakesPlaced() {
         <>
         <div className = {`centerContent ${classes.leftPanel}`}>
             <div className = {classes.topPanel}>
-                <Tent funct = {SetSteke} />
+                <Tent funct = {SetStake} />
             </div>
             <div className = {classes.bottomPanel}>
                 <Panel precent = {setDeepPres} precentSlider = {deepPres}/>
