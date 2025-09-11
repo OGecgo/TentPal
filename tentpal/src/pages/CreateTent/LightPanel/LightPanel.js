a0f256012ba91dbab2e43b43f317a4bdcad4c9ba
OGecgo <oian72116@gmail.com>
2025-09-11 15:21:07 +0300

    aply

--- tentpal/src/pages/CreateTent/LightPanel/LightPanel.js
+++ tentpal/src/pages/CreateTent/LightPanel/LightPanel.js
@@ -1,5 +1,6 @@
 import classes from './LightPanel.module.css';
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 import LeftPanel from 'components/LeftPanel/LeftPanel';
 import Header from 'components/Header/Header';
@@ -64,7 +65,7 @@ function LightPanel() {
             // Blue to Cyan
             r = 0;
             g = value - 512;
-            b = 255;
+            b = 255;    
         }
         else if (value < 1024) {
             // Cyan to Green
@@ -104,19 +105,19 @@ function LightPanel() {
                     <div className={classes.upperPanel}>
                         <MessageBox message={"Παρακαλώ επιλέξτε είδος φωτισμού"} width="calc(100% - 40px)" height={"30%"} top={"10%"} left={"20px"} />
                         <div className={classes.blockData}>
-                            <div className={classes.blockDataSize}>
+                            <div className={classes.blockDataSizeUp}>
                                 <MessageBox backgroundColor={"rgba(0, 0, 0, 0)"} message={"Σύνθετος Φωτισμός"} height={"50%"} width={"50%"} />
                             </div>
-                            <div className={classes.blockDataSize}>
+                            <div className={classes.blockDataSizeUp}>
                                 <MessageBox backgroundColor={"rgba(0, 0, 0, 0)"} message={"Νυχτερινό Φως"} height={"50%"} width={"50%"} />
                             </div>
-                            <div className={classes.blockDataSize}>
+                            <div className={classes.blockDataSizeUp}>
                                 <label className={classes.labelLeft}> on </label>
                                 <input className={classes.blockCheckBox} type="checkbox" id="special-light2-toggle" checked={SimpleLight} onChange={() => changeLightMode('simple')} />
                                 <label className={classes.labelRight}> off </label>
                                 <input className={classes.blockCheckBox} type="checkbox" id="special-light1-toggle" checked={!SimpleLight} onChange={() => changeLightMode('complex')} />
                             </div>
-                            <div className={classes.blockDataSize}>
+                            <div className={classes.blockDataSizeUp}>
                                 <label className={classes.labelLeft}> on </label>
                                 <input className={SimpleLight ? ` ${classes.blockCheckBox} ${classes.blockCheckBoxOff}` : classes.blockCheckBox} type="checkbox" id="special-light2-toggle" disabled={SimpleLight} checked={!SimpleLight && NightLight} onChange={() => { setNightLight(true); setColor('#FFFFFF'); setIntensity(0.3); }} />
                                 <label className={classes.labelRight}> off </label>
@@ -129,24 +130,24 @@ function LightPanel() {
                     <div className={classes.midlePanel}>
                         <MessageBox message={"Color Options"} width="calc(100% - 40px)" height={"30%"} top={"10%"} left={"20px"} />
                         <div className={classes.blockData}>
-                            <div className={classes.blockDataSize}>
+                            <div className={classes.blockDataSizeUp}>
                                 <MessageBox backgroundColor={"rgba(0, 0, 0, 0)"} message={"Επιλογή χρώματος"} height={"50%"} width={"50%"} />
                             </div>
-                            <div className={classes.blockDataSize}>
+                            <div className={classes.blockDataSizeUp}>
                                 <MessageBox backgroundColor={"rgba(0, 0, 0, 0)"} message={"Ένταση"} height={"50%"} width={"50%"} />
                             </div>
-                            <div className={classes.blockDataSize}>
+                            <div className={classes.blockDataSizeUp}>
                                 <input className={SimpleLight ? `blockSlider ${classes.rainbowLine}` : `blockSlider ${classes.whiteLine}`} type="range" id="opacityRange" min="0" max="1535" step="1" value={valueColor} onChange={SimpleLight ? (e) => setNewColor(Number(e.target.value)) : (e) => { setColor("#FFFFFF"); setValueColor(Number(e.target.value)) }} />
                             </div>
-                            <div className={classes.blockDataSize}>
+                            <div className={classes.blockDataSizeUp}>
                                 <input className={"blockSlider"} type="range" id="opacityRange" min="0.5" max="1" step="0.01" value={intensity} disabled={NightLight} onChange={(e) => setIntensity(Number(e.target.value))} />
                             </div>
                         </div>
                         <div className={`${classes.blockData} ${classes.blockBottom}`}>
-                            <div className={classes.blockDataSize}>
+                            <div className={classes.blockDataSizeDown}>
                                 <MessageBox backgroundColor={"rgba(0, 0, 0, 0)"} message={"Εκδηλώσεις"} height={"50%"} width={"50%"} />
                             </div>                        
-                            <div className={classes.blockDataSize}>
+                            <div className={classes.blockDataSizeDown}>
                                 <div className={SimpleLight ? `${classes.purpleBox} ${classes.colorBox}` : `${classes.colorBox}`} onClick={() => {if (SimpleLight)setNewColor(272 );}}></div>
                                 <div className={SimpleLight ? `${classes.blueBox  } ${classes.colorBox}` : `${classes.colorBox}`} onClick={() => {if (SimpleLight)setNewColor(649 );}}></div>
                                 <div className={SimpleLight ? `${classes.greenBox } ${classes.colorBox}` : `${classes.colorBox}`} onClick={() => {if (SimpleLight)setNewColor(925 );}}></div>
@@ -154,8 +155,8 @@ function LightPanel() {
                                 <div className={SimpleLight ? `${classes.orangeBox} ${classes.colorBox}` : `${classes.colorBox}`} onClick={() => {if (SimpleLight)setNewColor(1386);}}></div>
                             </div>
                         </div>
+                        <Link to = {"#"} className={classes.linkAplhy}>Aply</Link>
                     </div>
-
                 </div>
 
 
